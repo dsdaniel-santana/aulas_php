3bc2629b7bca48c3abd392cf5afdf08af1808a0d
dsdaniel-santana <daniel_santana_87@hotmail.com>
2023-05-02 14:45:20 -0300

    Create 1_sintaxe_geral.php

---
+++ 1_sintaxe_geral.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sintaxe Geral do PHP</title>
+</head>
+<body>
+<h1>Experimentando PHP</h1>
+<hr>
+
+<?php
+echo "Olá PHP!";
+echo "Teste!";
+
+$escola = "Senac";
+echo $escola;
+
+//CTRL; -> Comentários de uma linha 
+
+/*Alt+Shift+A -> Comentários de múltiplas linhas*/
+
+?>
+
+
+</body>
+</html>
